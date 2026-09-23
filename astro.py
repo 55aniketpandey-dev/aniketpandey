ad87c63507ecdb5579e3677e0c866be243c8f71b
55aniketpandey-dev <55aniket.pandey@gmail.com>
2026-09-23 11:52:18 +0530

    Update astro.py

--- astro.py
+++ astro.py
@@ -6,8 +6,7 @@ from groq import Groq
 # ==========================
 
 client = Groq(
-    api_key=["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"]
-)
+    api_key="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")
 
 SYSTEM_INSTRUCTION = """
 You are AstroAdvisor AI.
