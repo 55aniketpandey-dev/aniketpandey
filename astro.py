2af0df9f1a8fe753a959f12b520e6f7ece9df0e3
55aniketpandey-dev <55aniket.pandey@gmail.com>
2026-09-23 11:55:20 +0530

    Update astro.py

--- astro.py
+++ astro.py
@@ -202,7 +202,7 @@ with tab2:
                 try:
 
                     response = client.chat.completions.create(
-                        model="llama-3.3-70b-versatile",
+                        model="llama-3.1-8b-instant",
                         messages=[
                             {"role": "system", "content": SYSTEM_INSTRUCTION},
                             {"role": "user", "content": final_prompt}
